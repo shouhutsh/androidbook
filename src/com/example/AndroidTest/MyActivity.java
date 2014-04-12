9dfc59a3af52ca5d4ca86396cff0906ad54838ce
shouhutsh <shouhutsh@gmail.com>
2014-04-12 18:31:19 +0800

    第三章，数据库操作，注意里边AutoCompleteTextView控件的使用，以及动态添加TableRow的方法

--- src/com/example/AndroidTest/MyActivity.java
+++ src/com/example/AndroidTest/MyActivity.java
@@ -1,61 +1,25 @@
 package com.example.AndroidTest;
 
 import android.app.Activity;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.Bundle;
-import android.os.SystemClock;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
 
 public class MyActivity extends Activity {
-	private MyResponseReceiver receiver;
+	protected PetDatabaseHelper petDB = null;
 	/**
 	 * Called when the activity is first created.
 	 */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
 
-		IntentFilter filter = new IntentFilter(MyResponseReceiver.ACTION_RESP);
-		filter.addCategory(Intent.CATEGORY_DEFAULT);
-		receiver = new MyResponseReceiver();
-		registerReceiver(receiver, filter);
+		petDB = new PetDatabaseHelper(this);
 	}
 
 	@Override
 	public void onDestroy(){
-		this.unregisterReceiver(receiver);
 		super.onDestroy();
-	}
-
-	public void onMainUIThread(View v){
-		EditText et = (EditText) findViewById(R.id.edit);
-		TextView tv = (TextView) findViewById(R.id.show);
-
-		SystemClock.sleep(1500);
-
-		tv.setText(et.getText());
-	}
-
-	public void onIntentService(View v){
-		EditText et = (EditText) findViewById(R.id.edit);
-
-		Intent intent = new Intent(this, SimpleIntentService.class);
-		intent.putExtra(SimpleIntentService.IN, et.getText().toString());
-		startService(intent);
-	}
-
-	public class MyResponseReceiver extends BroadcastReceiver{
-		public final static String ACTION_RESP = "com.mamlambo.intent.action.MESSAGE_PROCESSED";
-		@Override
-		public void onReceive(Context context, Intent intent) {
-			TextView show = (TextView) findViewById(R.id.show);
-			show.setText(intent.getStringExtra(SimpleIntentService.OUT));
+		if(petDB != null){
+			petDB.close();
 		}
 	}
 }
---
+++ src/com/example/AndroidTest/PetAddActivity.java
@@ -0,0 +1,7 @@
+package com.example.AndroidTest;
+
+/**
+ * Created by Qi_2 on 14-4-12.
+ */
+public class PetAddActivity {
+}
